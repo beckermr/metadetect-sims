0c0989076a52e4ec726adb81bbbebac70485975c
Matthew R Becker <becker.mr@gmail.com>
2019-03-04 11:16:14 -0600

    more tests

---
+++ runs/run_galexp_psfps_trunc1_hmg_shearscene/config.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    'gal_type': 'exp',
+    'psf_type': 'ps',
+    'shear_scene': True,
+    'n_coadd': 30,
+    'n_coadd_psf': 1,
+    'homogenize_psf': True,
+    'ps_kws': {'trunc': 1.0}
+}
