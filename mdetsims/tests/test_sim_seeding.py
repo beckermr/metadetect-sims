384da25b62d18a78a9a7f5cd9d7d439fe4c8a51f
Matthew R Becker <becker.mr@gmail.com>
2019-03-05 09:22:59 -0600

    again

--- mdetsims/tests/test_sim_seeding.py
+++ mdetsims/tests/test_sim_seeding.py
@@ -67,3 +67,36 @@ def test_sim_seeding_not_reproduce(
     if psf_type != 'gauss':
         assert not np.array_equal(
             mbobs1[0][0].psf.image, mbobs2[0][0].psf.image)
+
+
+@pytest.mark.parametrize('gal_type', ['exp'])
+@pytest.mark.parametrize('psf_type', ['gauss', 'ps'])
+@pytest.mark.parametrize('homogenize_psf', [False, True])
+@pytest.mark.parametrize('n_coadd_psf', [1, 2, 3])
+def test_sim_seeding_shears(
+        gal_type, psf_type, homogenize_psf, n_coadd_psf):
+
+    s1 = Sim(
+        rng=np.random.RandomState(seed=10),
+        gal_type=gal_type,
+        psf_type=psf_type,
+        homogenize_psf=homogenize_psf,
+        n_coadd_psf=n_coadd_psf,
+        n_coadd=10,
+        g1=0.02)
+
+    s2 = Sim(
+        rng=np.random.RandomState(seed=10),
+        gal_type=gal_type,
+        psf_type=psf_type,
+        homogenize_psf=homogenize_psf,
+        n_coadd_psf=n_coadd_psf,
+        n_coadd=10,
+        g1=-0.02)
+
+    mbobs1 = s1.get_mbobs()
+    mbobs2 = s2.get_mbobs()
+
+    assert not np.array_equal(mbobs1[0][0].image, mbobs2[0][0].image)
+    assert np.array_equal(mbobs1[0][0].noise, mbobs2[0][0].noise)
+    assert np.array_equal(mbobs1[0][0].psf.image, mbobs2[0][0].psf.image)
