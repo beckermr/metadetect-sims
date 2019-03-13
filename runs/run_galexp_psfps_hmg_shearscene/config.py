cefdbc744c7eda6928a0acb9e1f4baa98edc2c42
Matthew R Becker <becker.mr@gmail.com>
2019-03-13 15:51:27 -0500

    try this crazy model

---
+++ runs/run_galexp_psfps_hmg_shearscene/config.py
@@ -0,0 +1,10 @@
+CONFIG = {
+    'gal_type': 'exp',
+    'psf_type': 'ps',
+    'shear_scene': True,
+    'n_coadd': 30,
+    'scale': 0.263,
+    'n_coadd_psf': 1,
+    'homogenize_psf': True,
+    'psf_kws': {'variation_factor': 5}
+}
