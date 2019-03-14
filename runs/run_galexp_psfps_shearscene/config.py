c64c25ce13a4c3f2edb076c7d9c1914dda1b89f0
Matthew R Becker <becker.mr@gmail.com>
2019-03-13 21:45:58 -0500

    ps no hmg

---
+++ runs/run_galexp_psfps_shearscene/config.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    'gal_type': 'exp',
+    'psf_type': 'ps',
+    'shear_scene': True,
+    'n_coadd': 30,
+    'scale': 0.263,
+    'n_coadd_psf': 1,
+    'psf_kws': {'variation_factor': 5}
+}
