fixed bug in PSF normalization

mdetsims/sim_utils.py
@@ -78,6 +78,12 @@ class Sim(dict):
     get_psf_obs(*, x, y):
         Get an ngmix Observation of the PSF at the position (x, y).
 
+    Attributes
+    ----------
+    area_sqr_arcmin : float
+        The effective area simulated in square arcmin assuming the pixel
+        scale is in arcsec.
+
     Notes
     -----
     The valid kinds of galaxies are
@@ -129,6 +135,8 @@ class Sim(dict):
         self.n_coadd_psf = n_coadd_psf or n_coadd
         self.homogenize_psf = homogenize_psf
         self.mask_and_interp = mask_and_interp
+
+        self.area_sqr_arcmin = ((self.dim - 2*self.buff) * scale / 60)**2
 
         self._galsim_rng = galsim.BaseDeviate(
             seed=self.rng.randint(low=1, high=2**32-1))
@@ -469,7 +477,7 @@ class Sim(dict):
 
         psf = galsim.Sum([
             p.getPSF(galsim.PositionD(x=x, y=y))
-            for p in self._psfs])
+            for p in self._psfs]).withFlux(1)
         psf_im = psf.drawImage(nx=21, ny=21, wcs=_psf_wcs).array.copy()
         psf_im /= np.sum(psf_im)
 
@@ -486,7 +494,7 @@ class Sim(dict):
 
         psf = galsim.Sum([
             p.getPSF(galsim.PositionD(x=x, y=y))
-            for p in self._psfs])
+            for p in self._psfs]).withFlux(1)
         psf_im = psf.drawImage(
             nx=21, ny=21, wcs=_psf_wcs, method='no_pixel').array.copy()
         psf_im /= np.sum(psf_im)

sim_utils/run_sim.py
@@ -9,12 +9,14 @@ from mdetsims import Sim, TEST_METADETECT_CONFIG
 from metadetect.metadetect import Metadetect
 from config import CONFIG
 
-for lib in ['ngmix', 'metadetect', 'mdetsims']:
+for lib in [__name__, 'ngmix', 'metadetect', 'mdetsims']:
     lgr = logging.getLogger(lib)
     hdr = logging.StreamHandler(sys.stdout)
     hdr.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
     lgr.setLevel(logging.DEBUG)
     lgr.addHandler(hdr)
+
+LOGGER = logging.getLogger(__name__)
 
 try:
     from mpi4py import MPI
@@ -125,18 +127,24 @@ def _run_sim_mdet(seed):
         config.update(TEST_METADETECT_CONFIG)
 
         rng = np.random.RandomState(seed=seed + 1000000)
-        mbobs = Sim(
-            rng=rng, g1=0.02, **CONFIG).get_mbobs()
+        sim = Sim(rng=rng, g1=0.02, **CONFIG)
+        mbobs = sim.get_mbobs()
         md = Metadetect(config, mbobs, rng)
         md.go()
         pres = _meas_shear(md.result)
 
+        dens = len(md.result['noshear']) / sim.area_sqr_arcmin
+        LOGGER.info('found %f objects per square arcminute', dens)
+
         rng = np.random.RandomState(seed=seed + 1000000)
-        mbobs = Sim(
-            rng=rng, g1=-0.02, **CONFIG).get_mbobs()
+        sim = Sim(rng=rng, g1=-0.02, **CONFIG)
+        mbobs = sim.get_mbobs()
         md = Metadetect(config, mbobs, rng)
         md.go()
         mres = _meas_shear(md.result)
+
+        dens = len(md.result['noshear']) / sim.area_sqr_arcmin
+        LOGGER.info('found %f objects per square arcminute', dens)
 
         return pres, mres
     except Exception: