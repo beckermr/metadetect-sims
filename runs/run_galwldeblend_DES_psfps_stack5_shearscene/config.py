a3ca46c26d7f51e2881ba07082d8deed413eaf77
Matthew R Becker <becker.mr@gmail.com>
2019-03-29 17:32:48 -0500

    run 5

---
+++ runs/run_galwldeblend_DES_psfps_stack5_shearscene/config.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    'gal_type': 'wldeblend',
+    'psf_type': 'ps',
+    'shear_scene': True,
+    'n_coadd': 30,
+    'scale': 0.263,
+    'n_coadd_psf': 5,
+    'gal_kws': {'survey_name': 'DES'}
+}
