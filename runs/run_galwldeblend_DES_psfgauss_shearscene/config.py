acf3f429d39611d629421aefc9f36076c7116690
Matthew R Becker <becker.mr@gmail.com>
2019-04-13 23:43:47 -0500

    new run types

--- runs/run_galwldeblend_DES_psfgauss_shearscene/config.py
+++ runs/run_galwldeblend_DES_psfgauss_shearscene/config.py
@@ -1,7 +1,9 @@
 CONFIG = {
-    'gal_type': 'ground_galsim_parametric',
+    'gal_type': 'wldeblend',
     'psf_type': 'gauss',
     'shear_scene': True,
     'n_coadd': 30,
     'scale': 0.263,
+    'n_coadd_psf': 1,
+    'gal_kws': {'survey_name': 'DES'},
 }
