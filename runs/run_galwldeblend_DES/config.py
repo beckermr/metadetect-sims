488a8d54a31531218dc0cd666b0e4ebf4fa282b7
Matthew R Becker <becker.mr@gmail.com>
2019-04-22 13:16:49 -0500

    new configs for latest runs

---
+++ runs/run_galwldeblend_DES/config.py
@@ -0,0 +1,11 @@
+CONFIG = {
+    'gal_type': 'wldeblend',
+    'psf_type': 'wldeblend',
+    'shear_scene': False,
+    'n_coadd': 10,  # per band
+    'scale': 0.263,
+    'n_coadd_psf': 1,
+    'gal_kws': {
+        'survey_name': 'DES',
+        'bands': ('r', 'i', 'z')},
+}
--- runs/run_galwldeblend_DES_psfgauss_shearscene/config.py
+++
@@ -1,9 +0,0 @@
-CONFIG = {
-    'gal_type': 'wldeblend',
-    'psf_type': 'gauss',
-    'shear_scene': True,
-    'n_coadd': 30,
-    'scale': 0.263,
-    'n_coadd_psf': 1,
-    'gal_kws': {'survey_name': 'DES'},
-}
