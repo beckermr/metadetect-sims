c80b34783fcfb56bf4dd4f3980209766e61aa444
Matthew R Becker <becker.mr@gmail.com>
2019-10-01 14:32:20 -0500

    faster

--- runs_end2end/run_end2end_galexp_psfgauss_shearscene/config.py
+++ runs_end2end/run_end2end_galexp_psfgauss_shearscene/config.py
@@ -2,9 +2,10 @@ CONFIG = {
     'gal_type': 'exp',
     'psf_type': 'gauss',
     'shear_scene': True,
-    'n_coadd': 10,
+    'n_coadd': 5,
     'scale': 0.263,
     'n_bands': 3,
 }
 
 DO_END2END_SIM = True
+N_PATCHES = 1_000_000
