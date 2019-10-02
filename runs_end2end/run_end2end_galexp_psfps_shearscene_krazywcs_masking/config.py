092137cd0e63bae21d1e411e536387b367c06ea3
Matthew R Becker <becker.mr@gmail.com>
2019-10-02 05:55:05 -0500

    new run

---
+++ runs_end2end/run_end2end_galexp_psfps_shearscene_krazywcs_masking/config.py
@@ -0,0 +1,15 @@
+CONFIG = {
+    'gal_type': 'exp',
+    'psf_type': 'ps',
+    'shear_scene': True,
+    'n_coadd': 5,
+    'scale': 0.263,
+    'n_bands': 3,
+    'position_angle_range': (0, 360),
+    'scale_frac_std': 0.03,
+    'wcs_shear_std': 0.03,
+    'wcs_dither_range': (-0.5, 0.5),
+}
+
+DO_END2END_SIM = True
+N_PATCHES = 1_000_000
