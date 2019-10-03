504a3ad19ab233e0ce4004376a28fba0e9ea3d7e
Matthew R Becker <becker.mr@gmail.com>
2019-10-03 15:12:36 -0500

    try again

--- runs_end2end/run_end2end_galexp_psfgauss_shearscene_kraywcs/config.py
+++ runs_end2end/run_end2end_galexp_psfgauss_shearscene_kraywcs/config.py
@@ -2,7 +2,7 @@ CONFIG = {
     'gal_type': 'exp',
     'psf_type': 'gauss',
     'shear_scene': True,
-    'n_coadd': 10,
+    'n_coadd': 5,
     'scale': 0.263,
     'n_bands': 3,
     'position_angle_range': (0, 360),
@@ -13,3 +13,4 @@ CONFIG = {
 }
 
 DO_END2END_SIM = True
+N_PATCHES = 1_000_000
--- runs_end2end/run_end2end_galexp_psfgauss_shearscene_masking/config.py
+++ runs_end2end/run_end2end_galexp_psfgauss_shearscene_masking/config.py
@@ -2,11 +2,11 @@ CONFIG = {
     'gal_type': 'exp',
     'psf_type': 'gauss',
     'shear_scene': True,
-    'n_coadd': 10,
+    'n_coadd': 5,
     'scale': 0.263,
     'n_bands': 3,
     'mask_and_interp': True,
-    'interpolation_type': 'cs-fourier',
+    'interpolation_type': 'cubic',
 }
 
 DO_END2END_SIM = True
