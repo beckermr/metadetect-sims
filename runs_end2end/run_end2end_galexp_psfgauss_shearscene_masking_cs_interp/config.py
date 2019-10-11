9136251b53e2c29b3c7a1c484dbf14f74dc84d61
Matthew R Becker <becker.mr@gmail.com>
2019-10-11 08:11:57 -0500

    another run

---
+++ runs_end2end/run_end2end_galexp_psfgauss_shearscene_masking_cs_interp/config.py
@@ -0,0 +1,12 @@
+CONFIG = {
+    'gal_type': 'exp',
+    'psf_type': 'gauss',
+    'shear_scene': True,
+    'n_coadd': 5,
+    'scale': 0.263,
+    'n_bands': 3,
+    'mask_and_interp': True,
+    'interpolation_type': 'cs-fourier',
+}
+
+DO_END2END_SIM = True
