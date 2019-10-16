1688042947267b8f979ac3d599043910ca62a1d3
Matthew R Becker <becker.mr@gmail.com>
2019-10-16 06:35:05 -0500

    new sim

---
+++ runs_end2end/run_end2end_galwldeblend_DES_psfps_shearscene_krazywcs_masking/config.py
@@ -0,0 +1,19 @@
+CONFIG = {
+    'gal_type': 'wldeblend',
+    'psf_type': 'ps',
+    'shear_scene': True,
+    'n_coadd': 10,
+    'scale': 0.263,
+    'n_bands': 3,
+    'position_angle_range': (0, 360),
+    'scale_frac_std': 0.03,
+    'wcs_shear_std': 0.03,
+    'wcs_dither_range': (-0.5, 0.5),
+    'mask_and_interp': True,
+    'interpolation_type': 'cs-fourier',
+    'gal_kws': {
+        'survey_name': 'DES',
+        'bands': ('r', 'i', 'z')},
+}
+
+DO_END2END_SIM = True
