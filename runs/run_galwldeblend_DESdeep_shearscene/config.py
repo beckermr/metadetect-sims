ead7af7e251d90cfd1ae8d326ed2a7e5a6c4786b
Matthew R Becker <becker.mr@gmail.com>
2019-10-31 07:43:02 -0500

    deep des runs

---
+++ runs/run_galwldeblend_DESdeep_shearscene/config.py
@@ -0,0 +1,12 @@
+CONFIG = {
+    'gal_type': 'wldeblend',
+    'psf_type': 'wldeblend',
+    'shear_scene': True,
+    'n_coadd': 10,  # per band
+    'scale': 0.263,
+    'n_coadd_psf': 1,
+    'gal_kws': {
+        'survey_name': 'DES',
+        'bands': ('r', 'i', 'z')},
+    'ngal_factor': 1,
+}
