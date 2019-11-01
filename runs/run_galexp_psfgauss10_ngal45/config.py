25459fc9fe41ac80464391e2fc1dbc2cd29f0123
Matthew R Becker <becker.mr@gmail.com>
2019-11-01 11:42:51 -0500

    try again

---
+++ runs/run_galexp_psfgauss10_ngal45/config.py
@@ -0,0 +1,11 @@
+CONFIG = {
+    'gal_type': 'exp',
+    'psf_type': 'gauss',
+    'shear_scene': False,
+    'n_coadd': 30,
+    'scale': 0.263,
+    'pff_kws': {'fwhm': 1.0},
+    'ngal': 45.0
+}
+
+N_PATCHES = 1_000_000
